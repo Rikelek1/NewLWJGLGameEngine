059e88f63b7817d6c58df4eaadd020ddd9109ae8
cynicalsaint <cynicalsaint.lol@gmail.com>
2015-01-26 15:10:17 +0000

    Initial commit

---
+++ src/net/rikelek1/engineTester/MainGameLoop.java
@@ -0,0 +1,41 @@
+package net.rikelek1.engineTester;
+
+import net.rikelek1.renderEngine.DisplayManager;
+import net.rikelek1.renderEngine.Loader;
+import net.rikelek1.renderEngine.RawModel;
+import net.rikelek1.renderEngine.Renderer;
+import org.lwjgl.opengl.Display;
+
+public class MainGameLoop {
+    public static void main(String[] args) {
+        DisplayManager.createDisplay();
+
+        Loader loader = new Loader();
+        Renderer renderer = new Renderer();
+
+        //OpenGL expects coords in CCW order by default
+        float[] vertices = {
+                -0.5f, 0.5f, 0,     //V0
+                -0.5f, -0.5f, 0,    //V1
+                0.5f, -0.5f, 0,     //V2
+                0.5f, 0.5f, 0       //V3
+        };
+
+        int[] indices = {
+                0, 1, 3,    //Top Left triangle (V0, V1, V3)
+                3, 1, 2     //Bottom right triangle (V3, V1, V2)
+        };
+
+        RawModel model = loader.loadToVAO(vertices, indices);
+
+        while(!Display.isCloseRequested()) {
+            renderer.prepare();
+            //game logic
+            renderer.render(model);
+            DisplayManager.updateDisplay();
+        }
+
+        loader.cleanUp();
+        DisplayManager.closeDisplay();
+    }
+}
